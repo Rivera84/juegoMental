cd5e3757073ae9bacc86c04bb5aafa5230e1ae37
Luisr <luisrivera084@gmail.com>
2019-11-01 00:00:03 -0600

    Creacion de pantalla del primer modo de juego, aun falta programar funcionalidad

---
+++ app/src/main/java/com/example/juegomental/modoFacil.kt
@@ -0,0 +1,12 @@
+package com.example.juegomental
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class modoFacil : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modo_facil)
+    }
+}
